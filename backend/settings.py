fe07ae95694780988e85a7beaef4eb1425662191
nemanjan00 <nemanjan00@gmail.com>
2018-10-19 16:18:50 +0200

    Configure db

--- backend/settings.py
+++ backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studservice',
+        'USER': 'root',
+        'PASSWORD': 'example',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
