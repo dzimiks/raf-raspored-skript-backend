cca151fb0ea7892db06ed14eb9dd527a431247af
nemanjan00 <nemanjan00@gmail.com>
2018-10-27 18:54:20 +0200

    Ruta za studente

--- studserviceapp/views.py
+++ studserviceapp/views.py
@@ -2,13 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from studserviceapp.models import Grupa, Nastavnik, Termin, RasporedNastave, Predmet,Nalog,Semestar,Student
 
+import time
+
 
 def timetableforuser(request,username):
     nalog = Nalog.objects.get(username=username)
     student = Student.objects.get(nalog=nalog)
-    grupa = Student.objects.get(grupa = student.grupa)
-    termini = []
-    termini.append(Termin.objects.get(grupe=grupa))
-    return HttpResponse(termini)
 
+    grupa = Grupa.objects.filter(student__id = student.id)
+
+    termini = []
+    termini = Termin.objects.filter(grupe__id = grupa[0].id)
+
+    resp = "<html><head><meta charset=\"UTF-8\"></head><body>"
+
+    for termin in termini:
+        resp += "<p>"
+        resp += termin.dan + " "
+        resp += termin.pocetak.strftime("%H:%M") + " "
+        resp += termin.zavrsetak.strftime("%H:%M") + " "
+        resp += termin.predmet.naziv + " "
+        resp += "<span style=\"float:right\">" + termin.nastavnik.ime + " " + termin.nastavnik.prezime + "</span>"
+        resp += termin.tip_nastave + " "
+        resp += "</p>"
+
+    resp += "</body></html>"
+
+    return HttpResponse(resp) 
 
